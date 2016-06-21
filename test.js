e327826d5c3103349058fe770ed82a833c8ee73e
Sergey Kozirev <kozirevss@gmail.com>
2016-06-21 13:56:08 +0300

    fix

--- test.js
+++ test.js
@@ -1,16 +1,19 @@
 var webdriverio = require('webdriverio');
 var options = {
 	desiredCapabilities: {
-		browserName: 'firefox'
+		browserName: 'chrome'
 	}
 };
 
 console.log('init');
 
-webdriverio
+var client = webdriverio
 	.remote(options)
-	.init()
-	.url('http://www.google.com')
+	.init();
+
+console.log(client, client.getTitle());
+
+client.url('http://www.google.com')
 	.getTitle().then(function(title) {
 		console.log('Title was: ' + title);
 	})
