5b5222a9faa03488a7d6e2c597a1fd79f25c4b98
Sergey Kozirev <kozirevss@gmail.com>
2016-06-21 14:43:30 +0300

    fix

--- test.js
+++ test.js
@@ -11,7 +11,10 @@ var client = webdriverio
 	.remote(options)
 	.init();
 
+
 console.log(client, client.getTitle());
+
+console.log(client.url('http://www.google.com').then(function() {console.log('opened');}));
 
 client.url('http://www.google.com')
 	.getTitle().then(function(title) {
